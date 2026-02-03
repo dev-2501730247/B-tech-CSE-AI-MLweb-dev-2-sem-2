987bf14250edb34720f04979f31e8dddeefd7f93
Dev Kaushik <dk0850244@gmail.com>
2026-02-03 12:08:53 +0530

    Add files via upload

---
+++ task-/script.js
@@ -0,0 +1,45 @@
+
+// Task - 1
+
+// const profile = document.getElementById('profile');
+
+// profile.style.backgroundColor = 'navy';
+// profile.style.color = 'white';
+// profile.style.padding = '15px';
+
+// profile.style.textAlign = 'center';
+
+
+// Task - 2
+
+// const x = document.getElementsByClassName("important");
+// console.log(x);
+
+// for (let i = 0; i < x.length; i++) {
+
+//     x[i].style.color = 'red';
+//     x[i].style.fontWeight = 'bold';
+//     x[i].style.fontSize = '15px';
+// }
+
+
+// task-3
+
+// const task3 = document.getElementsByTagName("p");
+// console.log(task3)
+
+// for(let i=0;i<task3.length;i++){
+//     if(i%2===0){
+//         task3[i].style.color='red'
+//     }
+//     else{
+//         task3[i].style.color='blue'
+//     }
+// }
+
+// task3[task3.length-1].style.fontWeight='bold'
+
+// Task - 4
+
+const task4 = document.querySelector(".box");
+task4.style.backgroundColor="cyan"
